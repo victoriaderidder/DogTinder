1c86cbe12c786bd0822e8c11c389f482c3b8db74
masst269 <massonit@oregonstate.edu>
2019-12-08 13:57:26 -0800

    Update portfolio.php

--- portfolio.php
+++ portfolio.php
@@ -21,24 +21,16 @@
     if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
         header("location: login.php");
         exit;
-    }
+    }elseif(isset($_SESSION["username"]) ){
     $username = $_SESSION["username"];
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
+    echo "$username";
     // query to select all information from supplier table
-	$query = "SELECT * FROM Dog_Profile WHERE Username=?";
-       if($stmt = mysqli_prepare($link,$query)){
-		// Bind variables to the prepared statement as parameters
-      		mysqli_stmt_bind_param($stmt, "s", $param_username);
-                  // Set parameters
-                  $param_username = $username;
-		   // Attempt to execute the prepared statement
-                   if(mysqli_stmt_execute($stmt)){
-			  // Store result
-                          mysqli_stmt_store_result($stmt);
-	if(mysqli_stmt_num_rows($stmt) > 0){ 
-		 mysqli_stmt_bind_result($stmt, $username);
-  		if(mysqli_stmt_fetch($stmt)){
+	$query = "SELECT * FROM Dog_Profile WHERE Username='$username'";
+	$result = mysqli_query($link, $query);
+	if (!$result) {
+		die("Query to show fields from table failed");
+	}
+	if(mysqli_num_rows($result) > 0){
         echo "<h1>Dog_Profile</h1>";  
 		echo "<table id='t01' border='1'>";
         echo "<thead>";
@@ -63,18 +55,20 @@
         }
         echo "</tbody>";                            
         echo "</table>";
-	}// Free result set
+		// Free result set
         mysqli_free_result($result);
-    } else{
-        header("location: welcome.php");
-        exit;
-    } 
-	mysqli_close($conn);
+        }else{
+            header("location: welcome.php");
+            exit;
+        } 
+    mysqli_close($link);
+}else{
+    echo("Query failed try again latter.");
+    exit;
+}
     ?>
             <p>
                 <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
             </p>
 </body>
 </html>
-
-	
