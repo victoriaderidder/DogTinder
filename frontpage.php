175610736c93b998377ab4a9aabb270dba7b3dfc
Victoria Fisher <50275233+vtoriafisher@users.noreply.github.com>
2019-12-11 17:52:53 -0800

    Add files via upload

---
+++ frontpage.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Dog Tinder</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+	<link rel="stylesheet" href="overall.css">
+	<link rel="stylesheet" href="frontpage.css">
+</head>
+<body>
+	<div id=cent style="background-image: url('https://i.imgur.com/dsX2uqD.jpg')";>
+		<div id="welcomeheader">
+			<h2> Welcome to Dog Tinder </h2>
+		</div>
+		<div id="loginbutton">
+			<a href="login.php"><button type="button" class="btn btn-primary btn-lg">Log In</button></a>
+		</div>
+		<div id="registerbutton">
+			<a href="register.php"><button type="button" class="btn btn-primary btn-lg">Register</button></a>
+		</div>
+	</div>
+</body>
