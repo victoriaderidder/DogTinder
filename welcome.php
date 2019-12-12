510043eed7cd4c9cc1cb74c412746ec22bdebb32
Victoria Fisher <50275233+vtoriafisher@users.noreply.github.com>
2019-12-11 18:26:35 -0800

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <link rel="stylesheet" href="overall.css">
+</head>
+<body>
+    <div id="cent">
+        <center><h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
+        <p><a href="portfolio.php" class="btn btn-primary">Your Dog</a><p><p>
+        <a href="profilecreation.php" class="btn btn-primary">Create New Dog</a><p>
+        <a href="swipe.php" class="btn btn-primary">Swipe On Dogs</a><p>
+        <a href="loves.php" class="btn btn-primary">Dogs You Love</a><p>
+        <a href="logout.php" class="btn btn-primary">Sign Out of Your Account</a></center>
+    </div>
+</body>
+</html>
