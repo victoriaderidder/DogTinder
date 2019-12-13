491a74d0ad76f669888cd091aaf5a3ab0aa7ad29
masst269 <massonit@oregonstate.edu>
2019-12-12 16:57:29 -0800

    Update trigger.sql

--- trigger.sql
+++ trigger.sql
@@ -1,1 +1,9 @@
-
+DELIMITER $$
+ CREATE TRIGGER `Before_Insert_Location` BEFORE INSERT ON `Visited`
+ FOR EACH ROW BEGIN
+  IF (EXISTS(SELECT 1 FROM Visited d WHERE d.Username = NEW.Username && d.Location_ID = New.Location_ID)) THEN
+    SIGNAL SQLSTATE VALUE '45000' SET MESSAGE_TEXT = 'INSERT failed due to duplicate mobile number';
+    END IF;
+END;
+$$
+DELIMITER ;
