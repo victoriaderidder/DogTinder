8d0c67f129b46d6fdc91e9e96936e80576aa111e
masst269 <massonit@oregonstate.edu>
2019-12-12 17:56:51 -0800

    Update trigger.sql

--- trigger.sql
+++ trigger.sql
@@ -10,3 +10,14 @@ DELIMITER $$
 END;
 $$
 DELIMITER ;
+
+--Trigger for Dog_Profile--
+DELIMITER $$
+ CREATE TRIGGER `Before_Insert_Dog` BEFORE INSERT ON `Dog_Profile`
+ FOR EACH ROW BEGIN
+  IF (EXISTS(SELECT 1 FROM Dog_Profile d WHERE d.Username = NEW.Username)) THEN
+SIGNAL SQLSTATE VALUE '45000' SET MESSAGE_TEXT = 'INSERT failed due to duplicate mobile number';
+END IF;
+END;
+$$
+DELIMITER ;
